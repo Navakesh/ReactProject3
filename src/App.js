4cf86f47696fa567f8e0fd5b96880489e7ec4d70
Navakesh <navakesh42@gmail.com>
2024-05-11 09:16:06 +0530

    Add user form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React,{useState} from 'react';
-import AddUser from './Components/Users/AddUser';
-import UsersList from './Components/Users/UsersList';
+import AddUser from "./Components/Users/AddUser"
+import React from "react";
 
 function App() {
-  const[usersList,setUsersList]=useState([])
-  
-  function adduserHandler(Uname, Uage, Ucollege){ 
-      setUsersList((previousList)=>{
-        return [...previousList,{name:Uname, age:Uage, college: Ucollege, id:Math.random().toString()}] 
-      })
-  }
-
   return (
     <div>
-      <AddUser onAddUser={adduserHandler} />
-      <UsersList users={usersList} />
+      <AddUser />
     </div>
   );
 }
